71ce3fcee469295b14003dcea145a409a49eb116
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-07-29 15:56:56 +0300

    create functions: input_from_excel, get_currency_rates, get_stock_prices, greetings

--- src/hlp.py
+++ src/hlp.py
@@ -43,31 +43,26 @@ file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
-# def input_from_excel(input_xlsx_file: str) -> list[Any]:
-#     """Функция принимает на вход путь до файла xlsx и возвращает список словарей"""
-#
-#     input_xlsx_file = pd.read_excel(abs_xlsx_path)
-#
-#     try:
-#         logger.info("Данные из файла xlsx импортированы")
-#
-#         # Преобразуем DataFrame в список словарей
-#         df_dict = input_xlsx_file.to_dict("records")
-#         output_list_dicts = []
-#         for i in df_dict:
-#             output_list_dicts.append(i)
-#         return output_list_dicts
-#     except Exception:
-#         logger.warning("Импортируемый список пуст или отсутствует.")
-#         return []
-#
-#
-# input_df = input_from_excel(abs_xlsx_path)
-#
-# input_xlsx_file = pd.read_excel(abs_xlsx_path)
-# print(input_xlsx_file)
-# print(input_from_excel(abs_xlsx_path))
+def input_from_excel(input_xlsx_file: str) -> list[Any]:
+    """Функция принимает на вход путь до файла xlsx и возвращает список словарей"""
 
+    input_xlsx_file = pd.read_excel(abs_xlsx_path)
+
+    try:
+        logger.info("Данные из файла xlsx импортированы")
+
+        # Преобразуем DataFrame в список словарей
+        df_dict = input_xlsx_file.to_dict("records")
+        output_list_dicts = []
+        for i in df_dict:
+            output_list_dicts.append(i)
+        return output_list_dicts
+    except Exception:
+        logger.warning("Импортируемый список пуст или отсутствует.")
+        return []
+
+
+input_df = input_from_excel(abs_xlsx_path)
 
 # input_xlsx_file = pd.read_excel(abs_xlsx_path)
 # df_dict = input_xlsx_file.to_dict("records")
@@ -83,53 +78,7 @@ logger.addHandler(file_handler)
 #     print(dict_test)
 
 
-# now = datetime.now()
-# print(now)
 
-
-# def greetings(input_datetime: str) -> str:
-#     date_update = dt.strptime(input_datetime, "%d.%m.%Y %H:%M:%S")
-#     time = date_update.strftime("%H:%M:%S")
-#
-#     if time >= "05:00:00" and time <= "12:00:00":
-#         return "Доброе утро"
-#     if time > "12:00:00" and time <= "18:00:00":
-#         return "Добрый день"
-#     if time > "18:00:00" and time <= "23:00:00":
-#         return "Добрый вечер"
-#     if time > "23:00:00" and time < "05:00:00":
-#         return "Доброй ночи"
-
-
-# print(greetings("01.01.2018 12:49:53"))
-
-#
-# input_xlsx_file = pd.read_excel(abs_xlsx_path)
-# df_dict = input_xlsx_file.to_dict("records")
-#
-# def sort_by_date(input_list: list[Any], descending=True) -> Any:
-#     """Функция сортировки операций по дате"""
-#
-#     sorted_list = sorted(input_list, key=lambda x: x.get("Дата операции"), reverse=descending)
-#
-#     for i in sorted_list:
-#         print(i)
-#
-# print(sort_by_date(df_dict))
-
-# def cards():
-#     input_date = str(input(
-# """Добрый день! Пожалуйста введите интересующую дату в диапозоне от 01.01.2018 12:49:53 до 14.02.2020 21:32:38 включительно в формате: YYYY-MM-DD HH:MM:SS"""))
-#     pattern = re.compile(r'\d{4}-\d{2}-\d{2}\s\d{2}-\d{2}-\d{2}')
-#     match = pattern.search(input_date)
-#     if match:
-#
-#         print("match")
-#     else:
-#         print('dddw1w')
-#
-#
-# cards()
 
 input_datetime = "2018-02-16 12:01:58"
 #
@@ -143,8 +92,6 @@ get_dt = get_data(input_datetime)
 print(f'get_data {get_dt}')
 
 
-#
-#
 # def greetings(input_datetime: str) -> str:
 #     date_update = dt.strptime(input_datetime, "%d.%m.%Y %H:%M:%S")
 #     time = date_update.strftime("%H:%M:%S")
@@ -171,28 +118,6 @@ print(f'get_data {get_dt}')
 #
 # print(print_i(input_df))
 
-
-# def data_update(file):
-#
-#
-#     df_date = pd.to_datetime(file['Дата операции'], format="%d.%m.%Y %H:%M:%S")
-#
-#     return df_date
-#
-# print(data_update(input_xlsx_file,"2018-02-16 00:00:00" ))
-
-
-# def from_excel(file):
-#     df = pd.read_excel(file)
-#     df_date = pd.to_datetime(df['Дата операции'], format="%d.%m.%Y %H:%M:%S")
-#     return df_date
-#
-# print(from_excel(abs_xlsx_path))
-#
-# # input_xlsx_file = pd.read_excel(abs_xlsx_path)
-# # df_date = pd.to_datetime(input_xlsx_file['Дата операции'], format="%d.%m.%Y %H:%M:%S")
-# # print(df_date)
-# # #print(input_xlsx_file)
 
 # def filter(file, datin):
 #     st = datin.replace(day=1, hour=0, minute=0, second=0)
