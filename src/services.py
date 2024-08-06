da1c5fef5d9a31ebad3b8345e28909780d2231ad
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-08-06 12:21:38 +0300

    Services is done

--- src/services.py
+++ src/services.py
@@ -12,7 +12,7 @@ from src.utils import format_date
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Создаем путь до файла логов относительно текущей директории
-rel_log_file_path = os.path.join(current_dir, "../logs/views.log")
+rel_log_file_path = os.path.join(current_dir, "../logs/services.log")
 abs_log_file_path = os.path.abspath(rel_log_file_path)
 
 
@@ -21,7 +21,7 @@ rel_xlsx_path = os.path.join(current_dir, "../data/operations.xlsx")
 abs_xlsx_path = os.path.abspath(rel_xlsx_path)
 
 # Добавляем логгер, который записывает логи в файл.
-logger = logging.getLogger("views")
+logger = logging.getLogger("services")
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler(abs_log_file_path, "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
@@ -30,6 +30,7 @@ logger.addHandler(file_handler)
 
 
 # Формируем список словарей, содержащий информацию о транзакциях - transactions
+logger.info("Cписок словарей, содержащий информацию о транзакциях (transactions) сформирован")
 df = pd.read_excel(abs_xlsx_path)
 df_draft = df[["Дата платежа", "Сумма платежа"]].copy(deep=True)
 df_clean = df_draft.dropna()
@@ -45,64 +46,36 @@ for i in df_output:
 
 
 def investment_bank(month: str, transactions: list[dict[str, Any]], limit: int) -> float:
-    """Функция принимает на вход аналищируемый месяц, список словарей с транзакциями, предел оуркгления
+    """Функция принимает на вход анализируемый месяц, список словарей с транзакциями, шаг округления
     и возвращает анализ инвестиционных накоплений в виде json-ответа"""
 
     result = 0
     for i in transactions:
         if month in i["Дата платежа"]:
             result += limit - (i["Сумма платежа"] % limit)
-
-    return round(result, 2)
+            result = round(result, 2)
+    logger.info(f"Сумма, которую удалось бы отложить в «Инвесткопилку» за {month} с шагом округления {limit} составляет {result}")
+    return result
 
 
 print(investment_bank("2021-12", transactions, 10))
 
 
-def home_page(input_df: pd.DataFrame) -> Any:
-    """Функция принимает на вход DataFrame и возвращает json-файл"""
 
-    # Формируем приветствие
-    logger.info("Приветствие сформировано")
-    greetings_output = greetings(input_datetime)
 
-    # Получаем начало месяца
-    begin_month = start_month(input_datetime)
-
-    # Отфильтровываем DataFrame по лимиту дат
-    logger.info("Входной DataFrame отфильтрован по лимиту дат")
-    df_date = pd.to_datetime(input_df["Дата операции"], format="%d.%m.%Y %H:%M:%S")
-    filtered_df_to_date = input_df[(begin_month <= df_date) & (df_date <= input_datetime)]
-
-    # Получаем требуемую информацию по картам (номер карты, общая сумма расходов, кэшбэк)
-    logger.info("Информация по банковским картам получена: последние 4 цифры карты, общая сумма расходов, кэшбэк")
-    cards_description = cards_info(filtered_df_to_date)
-
-    # Получаем информацию по ТОП-5 транзакциям по сумме платежа
-    logger.info("Информация по ТОП-5 транзакциям по сумме платежа получена")
-    top_five_transactions = top_transactions(filtered_df_to_date)
-
-    # Получаем информацию по курсам валют: USD, EUR
-    logger.info("Информация по курсам валют получена: USD, EUR")
-    currency_rates = get_currency_rates(abs_json_path)
-
-    # Получаем информацию по стоимости акций
-    logger.info("Информация по по стоимости акций получена")
-    stock_prices = get_stock_prices(abs_json_path)
-
-    # Формируем список словарей с результатами
-    result_list_dicts = {
-        "greeting": greetings_output,
-        "cards": cards_description,
-        "top_transactions": top_five_transactions,
-        "currency_rates": currency_rates,
-        "stock_prices": stock_prices,
-    }
-
-    # Формируем json-ответ
-    logger.info("json-ответ создан успешно")
-    json_output = json.dumps(result_list_dicts, ensure_ascii=False, indent=4)
-    return json_output
+    # # Формируем список словарей с результатами
+    # result_list_dicts = {
+    #     "greeting": greetings_output,
+    #     "cards": cards_description,
+    #     "top_transactions": top_five_transactions,
+    #     "currency_rates": currency_rates,
+    #     "stock_prices": stock_prices,
+    # }
+    #
+    # # Формируем json-ответ
+    # logger.info("json-ответ создан успешно")
+    # json_output = json.dumps(result_list_dicts, ensure_ascii=False, indent=4)
+    # return json_output
 
 
 # print(home_page(df))
